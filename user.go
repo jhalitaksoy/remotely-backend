5301d307ebc6d0687e9b3b7e1a96174115c06567
jhalitaksoy <jhalitaksoy@gmail.com>
2021-04-25 04:40:01 +0300

    removed user and room repositories

--- user.go
+++ user.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"errors"
-
 	"github.com/pion/webrtc/v2"
 )
 
@@ -11,85 +9,6 @@ type User struct {
 	ID        int
 	Name      string
 	Anonymous bool
-}
-
-//UserRepository is
-type UserRepository interface {
-	LoginUser(*User) bool
-	RegisterUser(*User) error
-	GetUserByID(int) *User
-	//NewUser() *User
-	NewAnonymousUser() *User
-	DeleteUser(ID int) bool
-}
-
-//var userRepository UserRepository = &UserRepositoryMock{lastUserID: -1}
-
-//UserRepositoryMock is
-type UserRepositoryMock struct {
-	users      []*User
-	lastUserID int
-}
-
-//RegisterUser is
-func (repo *UserRepositoryMock) RegisterUser(user *User) error {
-	for _, eachUser := range repo.users {
-		if eachUser.Name == user.Name {
-			return errors.New("user name already using")
-		}
-	}
-	repo.lastUserID = repo.lastUserID + 1
-	user.ID = repo.lastUserID
-	repo.users = append(repo.users, user)
-	return nil
-}
-
-//LoginUser is
-func (repo *UserRepositoryMock) LoginUser(user *User) bool {
-	/*for _, eachUser := range repo.users {
-		if eachUser.Name == user.Name {
-			if eachUser.Password == user.Password {
-				user.ID = eachUser.ID
-				return true
-			}
-		}
-	}*/
-	return false
-}
-
-//GetUserByID is
-func (repo *UserRepositoryMock) GetUserByID(ID int) *User {
-	for _, eachUser := range repo.users {
-		if eachUser.ID == ID {
-			return eachUser
-		}
-	}
-	return nil
-}
-
-//NewAnonymousUser ...
-func (repo *UserRepositoryMock) NewAnonymousUser() *User {
-	user := &User{}
-	user.Anonymous = true
-	repo.lastUserID = repo.lastUserID + 1
-	user.ID = repo.lastUserID
-	repo.users = append(repo.users, user)
-	return user
-}
-
-func (repo *UserRepositoryMock) DeleteUser(ID int) bool {
-	for i, eachUser := range repo.users {
-		if eachUser.ID == ID {
-			repo.users = removeUserByIndex(repo.users, i)
-			return true
-		}
-	}
-	return false
-}
-
-func removeUserByIndex(s []*User, i int) []*User {
-	s[i] = s[len(s)-1]
-	return s[:len(s)-1]
 }
 
 //RoomUser is
